ed8f533c7ded8893d5b86521628269340daf59b4
MarkJur-afk <jurgenmarkk@gmail.com>
2026-01-13 14:16:32 +0200

    Add project files.

---
+++ PiduApp/Models/Guest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PiduApp.Models
+{
+	public class Guest
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool WillAttend { get; set; }
+        public int PyhaId { get; set; } // Viide pühale, kuhu registreerutakse
+    }
+}
---
+++ PiduApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PiduApp.Startup))]
+namespace PiduApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
